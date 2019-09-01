efc6b89ef920ce0d3ee447cf343361733ea0e494
Eduardo <acorral@arkusnexus.com>
2019-08-31 22:48:23 -0700

    Created Room Database singleton service.

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,11 +17,9 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface VideoDao {
@@ -30,4 +28,42 @@ interface VideoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+/**
+ * This class is our actual database class.
+ *
+ * To establish we take 3 steps:
+ *
+ * 1) We extend [RoomDatabase]
+ * 2) We add an abstract DAO val ([VideoDao])
+ * 3) We add the Database annotation and introduce two parameters: entities (list of entities
+ * the database holds) and the version number (we're starting at 1)
+ */
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    /**
+     * Instance of DAO needed to gain access.
+     */
+    abstract val videoDao: VideoDao
+}
+
+/**
+ * Singleton instance of the DATABASE initialized in get method.
+ */
+private lateinit var INSTANCE: VideosDatabase
+
+/**
+ * Retrieves database object instance singleton. Initializes if not yet initialized.
+ */
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        // Check if needed to create within synchronized for thread safety
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    VideosDatabase::class.java,
+                    "videos").build()
+        }
+    }
+    return INSTANCE
 }
